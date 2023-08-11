80fd6485cd430cd0a62f43ff6e399b83019eea4e
SebasMedina22 <132634556+SebasMedina22@users.noreply.github.com>
2023-08-11 08:44:23 -0500

    Add files via upload

---
+++ Carro/src/Llanta.java
@@ -0,0 +1,17 @@
+public class Llanta {
+    private String marca;
+    private String tipoRin;
+
+    public Llanta(String marca, String tipoRin) {
+        this.marca = marca;
+        this.tipoRin = tipoRin;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getTipoRin() {
+        return tipoRin;
+    }
+}
